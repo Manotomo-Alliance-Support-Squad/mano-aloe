2df514319fae134114b390d58374a10e62f9aff9
Zapple Cat <zizibaluba@gmail.com>
2021-06-27 03:00:54 -0500

    Creating modal render

--- frontend/src/components/projectSection/imageCard.tsx
+++ frontend/src/components/projectSection/imageCard.tsx
@@ -5,25 +5,43 @@ import "./imageCard.css";
 
 interface ImageCardProps {
     thumbnail: string,
-    projectlink: string,
+    imageId: string,
+    modalCaption: string, 
 }
 
 
 export default class ImageCard extends Component<ImageCardProps>{
+    private readonly modalId: string;
+    private readonly modalImgSrcId: string;
+    private readonly modalCaption: string;
 
     constructor(props: ImageCardProps) {
         super(props);
+        this.modalId = this.props.imageId + "Modal"
+        this.modalImgSrcId = this.modalId + "Imgsrc"
+        this.modalCaption = this.modalId + "Caption"
+
     }
+
+    renderImgModal() {
+        var modal = document.getElementById(this.modalId);
+        if (!modal) {
+            return
+        } else {
+            modal.style.display = "block";
+        }
+    }
+
 
     render() {
         return (
             <div className="image-card-container">
                 <div className="project-card">
                     <div className="project-card-thumbnail-container">
-                        <img id="thumbnail" className="image-card-thumbnail" src={this.props.thumbnail} />
-                        <div id="modal" className="modal">
+                        <img id={this.props.imageId} className="image-card-thumbnail" src={this.props.thumbnail} />
+                        <div id={this.modalId} className="modal">
                             <span className="close">&times;</span>
-                            <img className="modal-content" id="img01" />
+                            <img className="modal-content" id={this.modalImgSrcId} />
                             <div id="caption"></div>
                         </div>
                     </div>
