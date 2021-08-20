467aecb7b2002aefd9265519ec2a7a59361c02a2
tduong <duongt@protonmail.com>
2021-08-19 22:49:30 -0700

    add easter egg and space balls reference

--- frontend/src/pages/error/404.tsx
+++ frontend/src/pages/error/404.tsx
@@ -41,8 +41,25 @@ export default class NotFoundPage extends React.Component<NotFoundPageProps, Not
                     404 not found!
                 </div>
                 <div className="error-not-found-content">
-                    hmmmmmmmmmmmmmmmmmmm
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/h6IwhTuKSRk?start=13" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
+                <div className="error-not-found-content">
+                    We combed the desert far and wide, but we ain't found shit!
+                </div>
+                Flexbox is really versatile. 
+                First, you can try reversing divs in JS and suddenly break everything because you suck, 
+                or have divs using text-align: center flash those perfectly centered texts, or have css 
+                polluting the homepage's CSS and putting them on like, "Haha, fsck'd your props!" 
+                That's just way too cute! Instead, flexboxes within flexboxes! I really like when flexbox have 
+                like 3 layers of display: flex, and it's amazing how it still fscking works. 
+                I really like how it can fulfill all those abstract needs. 
+                Being able to switch up the direction and orientation of flexbox based on your mood 
+                is a lot of fun too!
+                It's actually so much fun! You have a center-justified flexbox, or a flex-direction: reverse, 
+                everything! It's like you're enjoying all these kinds of flexbox at a buffet. 
+                I really want Zapple to try some on or Ringo to try some on to complement their text-align: center. 
+                We really need flexbox to become a thing everywhere and start using them for everything. 
+                Don't. You. Think. We. Really. Need. To. Officially. Give. Everyone. Flexbox?
             </div>
         )
     }
