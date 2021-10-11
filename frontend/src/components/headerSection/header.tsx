236c7788fa63e882d5885fa84d6eedbcbef49cbc
Zapple Cat <zizibaluba@gmail.com>
2021-10-11 02:18:43 -0500

    port 2020 aloe birthday renders with minor temp code for testing

--- frontend/src/components/headerSection/header.tsx
+++ frontend/src/components/headerSection/header.tsx
@@ -2,6 +2,11 @@ import { Component } from 'react';
 import '../../shared/globalStyles/global.css';
 import './header.css';
 
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import ArrowDropDownCircleOutlinedIcon from '@material-ui/icons/ArrowDropDownCircleOutlined';
+
+import Manotomo1 from '../../assets/sprites/manotomo1.png'
+import Manotomo2 from '../../assets/sprites/manotomo2.png'
 
 interface HeaderState {
 }
@@ -12,7 +17,7 @@ interface HeaderProps {
 
 export default class HeaderSection extends Component<HeaderProps, HeaderState>
 {
-    render() {
+    renderDefaultHeader(): JSX.Element {
         return (
             <header id="header" className="app-header">
                 <div className="community-message-card">
@@ -25,5 +30,39 @@ export default class HeaderSection extends Component<HeaderProps, HeaderState>
                 <div style={{height: 100}}/>
             </header>
         )
+    }
+    
+    renderBirthdayHeader(): JSX.Element {
+        return (
+            <>
+                <header className="app-header app-birthday-header">
+                    <div style={{height: 50}}/>
+                    <img id="manotomo1" src={Manotomo1}/>
+                    <img id="manotomo2" src={Manotomo2}/>
+                </header>
+                <div className="separator">
+                    <AnchorLink offset='120' href='#bday-card'>
+                        <ArrowDropDownCircleOutlinedIcon className="anchor-link" style={{width: 36, height:36}}/>
+                    </AnchorLink>
+                </div>
+                <div id="bday-card" className="justify-align-center bday-msg-container">
+                    <div className="community-message-card birthday-card">
+                        <h1 className="community-message-header">Happy Birthday, Aloe</h1>
+                        <div className="community-message-body text-center">
+                            <p>お誕生日おめでとうございます</p>
+                            <p>お元気でいらっしゃいますか</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    render() {
+        let today: Date = new Date();
+        // if (today.getDate() == 28 && today.getMonth() == 9) //Oct 28th
+        if (today)
+            return this.renderBirthdayHeader();
+        return this.renderDefaultHeader();
     }
 }
